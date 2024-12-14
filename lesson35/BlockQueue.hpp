208b57df620a3d42c8dc028ae6c56398e3d69828
asdfrewqr <1538077694@qq.com>
2024-12-14 21:19:38 +0800

    lesson35生成消费存储线程队列

--- lesson35/BlockQueue.hpp
+++ lesson35/BlockQueue.hpp
@@ -12,12 +12,13 @@ template<class T>
 class BlockQueue
 {
 public:
-    static const int gmaxcap=5;
+    static const size_t gmaxcap=5;
 public:
     
-    BlockQueue(const int& max=gmaxcap)
+    BlockQueue(size_t max=5)
     :_max(max)
     {
+        cout<<_max;
         pthread_mutex_init(&_lock,nullptr);
         pthread_cond_init(&_prod,nullptr);
         pthread_cond_init(&_cons,nullptr);
@@ -26,9 +27,12 @@ public:
     void push(const T& in)
     {
         pthread_mutex_lock(&_lock);
+        
         while(is_full())
         {
+           
             pthread_cond_wait(&_prod,&_lock);
+            // cout<<"满了"<<endl;
         }
         _data.push(in);
         pthread_cond_signal(&_cons);
@@ -38,9 +42,12 @@ public:
     void pop(T* out)
     {
         pthread_mutex_lock(&_lock);
+       
         while(is_empty())
         {
+            
             pthread_cond_wait(&_cons,&_lock);
+            
         }
         *out=_data.front();
         _data.pop();
@@ -53,18 +60,26 @@ public:
         pthread_cond_destroy(&_prod);
         pthread_cond_destroy(&_cons);
     }
+//     void print_queue() {
+//          cout<<_data.size()<<"MK"<<endl;
+//     while (!_data.empty()) {
+//         cout << " k"<<endl;
+//         _data.pop();
+//     }
+//     cout << endl;
+// }
     bool is_empty()
     {
         return _data.empty();
     }
     bool is_full()
     {
-        return _data.size()-_max==0;
+        return _data.size()==_max;
     }
 private:
     
     queue<T> _data;
-    int _max;
+    size_t _max;
     pthread_mutex_t _lock;
     pthread_cond_t _prod;
     pthread_cond_t _cons;
